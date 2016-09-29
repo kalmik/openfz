4ec142ad88914467eb5a56facae2fbf75db104ba
Sergio Filipe <sergio.kalmik@gmail.com>
2016-09-28 23:14:37 -0300

    input.jsx

--- edit-tool/app/src/components/input.jsx
+++ edit-tool/app/src/components/input.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import ReactHighcharts from 'react-highcharts';
 
-import fis from './fixture' 
-var Input = React.createClass ({
+import { Button, Tabs, Tab } from 'react-bootstrap';
 
-  getInitialState (){
+import fis from './fixture' 
+class Input extends React.Component {
+
+  constructor(...args) {
+    super(...args);
     var _series = []
 
     var funcs = fis.inputs[0].functions;
@@ -29,12 +33,14 @@ var Input = React.createClass ({
       range: fis.inputs[0].range
     };
 
-    return _state;
-  },
+    this.state = _state;
+  }
 
   getChartConfig () {
     return {
       chart: {
+        animation: false,
+        title: false,
         type: 'area',
         height: 200
       },
@@ -46,14 +52,31 @@ var Input = React.createClass ({
         min: 0,
         max: 1
       },
-      series: this.state.series
+      series: this.state.series,
+      plotOptions: {
+        series: {
+          animation: false
+        }
+      },
     }
-  },
+  }
 
   render() {
     console.log(this.state)
 
-    return <ReactHighcharts config={this.getChartConfig()}></ReactHighcharts>
+    return (
+      <section>
+        <ReactHighcharts config={this.getChartConfig()}></ReactHighcharts>
+        <Tabs defaultActiveKey={0} id="uncontrolled-tab-example">
+        {
+          this.state.functions.map( (el, i) => {
+            return <Tab eventKey={i} key={"input"+i} title={el.name}>{el.name}</Tab>
+          })
+        }
+        </Tabs>
+      </section>
+    )
   }
-})
+}
+
 ReactDOM.render(<Input/>, document.querySelector('#input-pane .panel-body'));
